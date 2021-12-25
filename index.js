7e46f47f79052d277d76c88877c67eb8e2790d6d
Arash <arashkaffashi@gmail.com>
2021-12-24 20:27:29 -0400

    Initial adjustment

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * @returns {String} either ‘Rock’, ‘Paper’ or ‘Scissors’
+ */
+function computerPlay() {
+    // unimplemented!
+    console.warn(`unimplemented! ${arguments.callee.name}`);
+}
+
+/** 
+ * Plays a single round of Rock Paper Scissors
+ * @param {String} playerSelection player's selection, it is case-insensitive
+ * @param {String} computerSelection
+ * @returns {String} declare winner
+ */
+function playRound(playerSelection, computerSelection) {
+    // unimplemented!
+    console.warn(`unimplemented! ${arguments.callee.name}`);
+}
+
+/**
+ * Runs the game 5 times
+ */
+function game() {
+    // unimplemented!
+    console.warn(`unimplemented! ${arguments.callee.name}`);
+}
